6e98b34206877fa64ae3f09e69ae51b4d1350cff
Miguelucho <miguelacho.0806@gmail.com>
2018-07-17 22:19:02 -0400

    rubric: *Using rtabmap database viewr at least 3 loop closures are found and the occupancy grid is identifiable 	 *The global 3D map reflects the characteristics of  the room 	 *Personal Gazebo world file is supplied 	 *A screenshot of robot mapping the personal Gazebo world is present
     file .gitignore:
    	/catkin_ws/.catkin_workspace
    	/catkin_ws/src/CMakeLists.txt
    	/catkin_ws/build/
    	/catkin_ws/devel/
    	/catkin_ws/src/udacity_map/CMakeLists.txt
    	/catkin_ws/src/udacity_map/package.xml
    	rtabmapKitchen.db
    	rtabmapBotWorld.db
    
     confirmed changes:
    	modified:     src/udacity_map/launch/localization.launch
    	modified:     src/udacity_map/launch/mapping.launch
    	added:        src/udacity_map/img
    	modified:     src/udacity_map/worlds/models/bot_world.world
    
     Cambios no rastreados para el commit:
    	modificado:     ../.gitignore

--- catkin_ws/build/udacity_map/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/workspaceROS/MapMyWorldRobot-Project/catkin_ws/src/udacity_map
-# Build directory: /home/nvidia/workspaceROS/MapMyWorldRobot-Project/catkin_ws/build/udacity_map
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
